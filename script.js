610b06b0a50079f8c4e3a6dec86fb86e4881e3b3
Yenaeo <freshis_yeeun@naver.com>
2025-12-23 20:48:37 +0900

    aaa

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // ============================================
 
 // Google Apps Script 웹앱 URL (배포 후 여기에 URL을 입력하세요)
-const APPS_SCRIPT_URL = 'Yhttps://script.google.com/macros/s/AKfycbyxxZXDetlbneDWfZNvvMBxSrX99s5E12cTGlJ32MkjVw2c28dcqnLASshEhX2WuJQk/execOUR_APPS_SCRIPT_WEB_APP_URL_HERE';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyxxZXDetlbneDWfZNvvMBxSrX99s5E12cTGlJ32MkjVw2c28dcqnLASshEhX2WuJQk/exec';
 
 // DOM이 로드된 후 실행
 document.addEventListener('DOMContentLoaded', function() {
